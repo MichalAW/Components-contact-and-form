7a1bfe2c731b8206128c9a86da8aa8e560ae87ae
MichalAW <michal.wanta@gmail.com>
2019-05-10 21:25:30 +0200

    add htmlFor

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -8,7 +8,7 @@ var ContactForm = React.createClass({
             React.createElement('form', {className: 'contactForm'},
 
             React.createElement('input', {
-                label: "name",
+                htmlFor: "name",
                 type: 'text',
                 placeholder: 'Name',
                 id: "inputYourName",
@@ -17,7 +17,7 @@ var ContactForm = React.createClass({
             }),
 
             React.createElement('input', {
-                label: "surname",
+                htmlFor: "surname",
                 type: 'text',
                 placeholder: 'Surname', 
                 id: "inputYourSurname",
@@ -26,7 +26,7 @@ var ContactForm = React.createClass({
             }),
 
             React.createElement('input', {
-                label: "email",
+                htmlFor: "email",
                 type: 'email',
                 placeholder: 'Email',
                 id: "inputYourEmail",
