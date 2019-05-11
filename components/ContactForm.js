c2442fd6880fc3e7b13eeb496d2b5f467457636b
MichalAW <michal.wanta@gmail.com>
2019-05-11 08:20:31 +0200

    fix

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -7,34 +7,44 @@ var ContactForm = React.createClass({
         return (
             React.createElement('form', {className: 'contactForm'},
 
-            React.createElement('input', {
-                htmlFor: "name",
-                type: 'text',
-                placeholder: 'Name',
-                id: "inputYourName",
-                required: "",
-                value: this.props.contact.firstName,
-            }),
+                React.createElement('label', {
+                    htmlFor: "inputYourName",
+                }, 'Name'), 
 
-            React.createElement('input', {
-                htmlFor: "surname",
-                type: 'text',
-                placeholder: 'Surname', 
-                id: "inputYourSurname",
-                required: "",
-                value: this.props.contact.lastName,
-            }),
+                React.createElement('input', {
+                    type: 'text',
+                    placeholder: 'Name',
+                    id: "inputYourName",
+                    required: "",
+                    value: this.props.contact.firstName,
+                }),
 
-            React.createElement('input', {
-                htmlFor: "email",
-                type: 'email',
-                placeholder: 'Email',
-                id: "inputYourEmail",
-                required: "",
-                value: this.props.contact.email,
-            }),
+                React.createElement('label', {
+                    htmlFor: "inputYourSurname",
+                }, 'Surname'), 
 
-            React.createElement('button', {type: 'submit'}, "Add contact")
+                React.createElement('input', {
+                    type: 'text',
+                    placeholder: 'Surname', 
+                    id: "inputYourSurname",
+                    required: "",
+                    value: this.props.contact.lastName,
+                }),
+
+                React.createElement('label', {
+                    htmlFor: "inputYourEmail",
+                }, 'Email'), 
+
+                React.createElement('input', {
+                    htmlFor: "email",
+                    type: 'email',
+                    placeholder: 'Email',
+                    id: "inputYourEmail",
+                    required: "",
+                    value: this.props.contact.email,
+                }),
+
+                React.createElement('button', {type: 'submit'}, "Add contact")
             )
         );
     },
